9f0786a94efda817332093d9a85fe4f13c21f1cd
YanKyawMinGTC <36588721+YanKyawMinGTC@users.noreply.github.com>
2020-10-08 16:02:45 +0630

    initialize project

---
+++ todosapp/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <toDos />
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import toDos from './components/todos.vue'
+
+export default {
+  name: 'App',
+  components: {
+    toDos
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ todosapp/src/components/todos.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>hello wold </h1>
+        <p>theiijijsadifjaisdf </p>
+
+    </div>
+</template>
